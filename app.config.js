e511df51d5a8f97af1c937190096440f48f1b2b2
Lefty <ugyensamdrupdorji@gmail.com>
2023-04-28 17:45:42 +0600

    Build fix

--- app.config.js
+++ app.config.js
@@ -2,6 +2,17 @@ import "dotenv/config";
 
 export default {
 	extra: {
+		eas: {
+			projectId: "6710408e-6e19-4c0e-938c-6283b14d4930",
+		},
 		GOOGLE_SSO: process.env.GOOGLE_SSO,
+	},
+	android: {
+		googleServicesFile: "./google-services.json",
+		adaptiveIcon: {
+			foregroundImage: "./assets/adaptive-icon.png",
+			backgroundColor: "#ffffff",
+		},
+		package: "com.leftylefty.yum",
 	},
 };
