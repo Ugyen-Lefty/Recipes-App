30e2f09d44fe48f03447515af0cd9ca6289f08c6
Lefty <ugyensamdrupdorji@gmail.com>
2023-05-01 18:19:29 +0600

    Location Fix

--- Screens/ProfileScreen.tsx
+++ Screens/ProfileScreen.tsx
@@ -1,6 +1,29 @@
 import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
 
 export default function ProfileScreen({ onsignOut, user }: any) {
+	const [location, setLocation] = useState(null);
+	const [placeName, setPlaceName] = useState(null);
+
+	useEffect(() => {
+		(async () => {
+			let { status } = await Location.requestForegroundPermissionsAsync();
+			if (status !== "granted") {
+				return;
+			}
+
+			const location = await Location.getCurrentPositionAsync({});
+			setLocation(location as any);
+
+			let place = await Location.reverseGeocodeAsync({
+				latitude: location.coords.latitude,
+				longitude: location.coords.longitude,
+			});
+			setPlaceName(place[0] as any);
+		})();
+	}, []);
+
 	return (
 		<View style={styles.container}>
 			<Image source={{ uri: user?.photoURL }} style={styles.profileImage} />
@@ -8,6 +31,17 @@ export default function ProfileScreen({ onsignOut, user }: any) {
 				<Text style={styles.name}>{user?.displayName}</Text>
 				<Text style={styles.email}>{user?.email}</Text>
 			</View>
+			{location && (
+				<Text style={styles.email}>
+					Logged in from:
+					<Text style={styles.locationText}>
+						{" " +
+							(placeName as any)?.city +
+							" , " +
+							(placeName as any)?.country}
+					</Text>
+				</Text>
+			)}
 			<TouchableOpacity style={styles.button} onPress={onsignOut}>
 				<Text style={styles.buttonText}>Logout</Text>
 			</TouchableOpacity>
@@ -56,5 +90,8 @@ const styles = StyleSheet.create({
 		color: "white",
 		fontSize: 18,
 		fontWeight: "bold",
+	},
+	locationText: {
+		fontSize: 16,
 	},
 });
